0cff0be870d8280e14ada49142bc653a49bf40ac
Yash Sanjay Adke <yash@Yashs-MacBook-Air.local>
2025-12-12 20:53:38 +0530

    Logo Changes

--- shopMart/settings.py
+++ shopMart/settings.py
@@ -124,12 +124,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+
 STATICFILES_DIRS = [
-    'shopMart/static',
+    BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product,Variation
+from .models import Product,Variation,ReviewRating
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
@@ -13,3 +13,5 @@ class VariationAdmin(admin.ModelAdmin):
     list_editable = ('is_active',)
     list_filter = ('product', 'variation_category', 'variation_value')
 admin.site.register(Variation, VariationAdmin)
+
+admin.site.register(ReviewRating)
